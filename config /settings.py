6d204242651735191cb2222aafa936fb7a206e09
fnaeher <78532611+fnaeher@users.noreply.github.com>
2024-11-12 20:12:15 +0100

    Create settings.py

---
+++ config /settings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 12:56:23 2024
+
+@author: AnatolFiete.Naeher
+"""
+
+path = "path\\"
+
+openAI_Model = "gpt-4o-mini"
+chunks = 5
+ctdwn = 1
+filename = 'CTD'
+batch_filename = 'b_output'
