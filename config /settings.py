cfd7ecaf617f377533c282ae07ca159944cf0e25
fnaeher <78532611+fnaeher@users.noreply.github.com>
2024-11-14 14:34:51 +0100

    Update settings.py

--- config /settings.py
+++ config /settings.py
@@ -5,6 +5,14 @@ Created on Tue Nov 12 12:56:23 2024
 @author: AnatolFiete.Naeher
 """
 
+param = {
+    'dbname': 'aact',
+    'user': "user\\",  
+    'password': "password",   
+    'host': 'aact-db.ctti-clinicaltrials.org',
+    'port': '5432'
+}
+
 path = "path\\"
 
 openAI_Model = "gpt-4o-mini"
